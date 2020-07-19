61a9567cf818baede808f14ff5db31a69a5a89af
lls1994 <1173967838@qq.com>
2020-07-19 19:45:00 +0800

    vuepress deploy

--- src/.vuepress/navConfig.js
+++ src/.vuepress/navConfig.js
@@ -1,32 +1,10 @@
 module.exports = [{
-    text: "前端",
+    text: "学习笔记",
     items: [{
-        text: "收藏",
-        link: "/pages/study/frontEnd/web"
-    }, {
-        text: "javaScript",
-        link: "/pages/study/frontEnd/javaScript"
-    }, {
-        text: "CSS",
-        link: "/pages/study/frontEnd/CSS"
-    }]
-}, {
-    text: "收藏",
-    items: [{
-        text: "学习",
+        text: "npm",
         link: "/pages/study/"
-
-    }, {
-        text: "娱乐",
-        link: "/pages/entertainment/"
     }]
-}, {
-    text: "vscode",
-    link: "/pages/study/vsCode"
-}, {
-    text: "翻墙",
-    link: "/pages/study/fanqiang"
-}, {
+},{
     text: "MDN",
     link: "https://developer.mozilla.org/zh-CN/"
 }]
